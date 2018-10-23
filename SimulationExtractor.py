4ffceb8526395e8da0e52be3851f21ad1c6da865
fabianobd <fabiano.dacunti@xmobots.com>
2018-10-23 10:07:28 -0300

    Added simulation result search

--- SimulationExtractor.py
+++ SimulationExtractor.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import re
 
 
 class SimulationExtractor:
@@ -9,25 +10,34 @@ class SimulationExtractor:
 
     @staticmethod
     def simulate(file):
+        # Initialize eldo
         init_eldo = "C:\\Windows\\SysWOW64\\cmd.exe /k \"\"D:\\WinPrograms\\AMS_16_2_patch3_win64\\bin\\ams_setup.bat" \
                     "\" \"D:\\WinPrograms\\AMS_16_2_patch3_win64\" eldo -help\""
         proc = subprocess.Popen(init_eldo, stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding='utf-8')
+
+        # Simulate
         stdout, stderr = proc.communicate('eldo "' + os.getcwd() + '\\' + file + '"\n')
 
         return stdout
 
-
     @staticmethod
     def get_sim_results(sim_output):
-        pass
+        find_term = "(?=Simulation completed)*([^\s]+)\s*=\s*([^\s]*)\s*"
+        result = re.findall(find_term, sim_output, re.IGNORECASE)
+        return result
 
 
 def main():
-    out = SimulationExtractor.simulate("latch.cir")
-    print(out)
-    file_out = open("out", "w")
-    file_out.write(out)
-    file_out.close()
+    results = SimulationExtractor.get_sim_results("Simulation completed\n\nTEMPO         =  3.658768E-10" \
+                                                  "\nTAU           =  4.23487E-3)")
+    print(results)
+    result_list = []
+    for result in results:
+        dic = dict()
+        dic[result[0]] = result[1]
+        result_list.append(dic)
+
+    print(result_list)
 
 
 if __name__ == "__main__":
